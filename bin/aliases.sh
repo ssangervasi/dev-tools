369a0a534ed93c3ec34694156b8648343865a4c3
ssangervasi <sebastian.sangervasi@gmail.com>
2016-07-02 22:10:39 -0700

    Alias pip in windows.

--- bin/aliases.sh
+++ bin/aliases.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 alias 'alias'='alias' #lol
 alias '..'='cd ..'
-alias '...'='cd ~/Documents/workspace &> /dev/null || cd /b/Documents/GitHub'
+
+# Get ... to work on my different machines
+work_osx='~/Documents/workspace'
+work_win='/b/Documents/GitHub'
+if (test -d $work_osx ); then
+	alias '...'='cd '$work_osx;
+elif (test -d $work_win ); then
+	alias '...'='cd '$work_win;
+else
+	alias '...'='cd $DEV_BIN'
+fi
 
 # Network
 alias 'shacme'='ssh seb@acme.ditto.com'
@@ -17,5 +27,10 @@ alias 'gitn'='git checkout -b'
 alias 'gitp'='git pull --ff-only'
 alias 'gits'='git status -sb'
 
-# Lock the screen (when going AFK)
+# Lock the screen (OSX only)
 alias afk='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
+
+# Alias pip (for windows)
+if (! which pip &> /dev/null); then
+	alias 'pip'='python -m pip'
+fi
