dfd5dd5b08e3ca1ddb7dafd64bb07369ee2be724
Sebastian Sangervasi <s.ebastian.sangervasi@gmail.com>
2018-12-14 16:31:05 -0800

    safer prompt command mangling

--- futility/promptimal.sh
+++ futility/promptimal.sh
@@ -42,14 +42,6 @@ cls() {
 # 	fi
 # }
 
-prompt_clock() {
-	export CHECK_SECONDS=$((SECONDS-CHECK_SECONDS))
-	export ELAPSED=$((CHECK_SECONDS/60)):$((CHECK_SECONDS%60))
-	export CHECK_SECONDS=$SECONDS
-}
-
-export prompt_swap_command=''
-export PROMPT_COMMAND=$PROMPT_COMMAND'; $prompt_swap_command'
 prompt_swap() {
 	export prompt_swap_command=''
 	simple_prompt='\$ '
@@ -60,7 +52,7 @@ prompt_swap() {
 			export PS1=$info_prompt
 			;;
 		-t|--timer)
-			export prompt_swap_command=prompt_clock
+			mangle_prompt_command "prompt_clock"
 			export PS1=$timer_prompt
 			;;
 		-s|--simple)
@@ -72,6 +64,29 @@ prompt_swap() {
 		*)
 			export PS1=$(default "$1" "$simple_prompt")
 	esac
+}
+
+prompt_clock() {
+	export CHECK_SECONDS=$((SECONDS-CHECK_SECONDS))
+	export ELAPSED=$((CHECK_SECONDS/60)):$((CHECK_SECONDS%60))
+	export CHECK_SECONDS=$SECONDS
+}
+
+mangle_prompt_command() {
+	export PROMPT_COMMAND_CUSTOM="$1"
+	if empty ${PROMPT_COMMAND_ORIGINAL}; then
+		export PROMPT_COMMAND_ORIGINAL="$PROMPT_COMMAND"
+		export PROMPT_COMMAND="prompt_command_function"
+	fi
+}
+
+unmangle_prompt_command() {
+	export PROMPT_COMMAND="$PROMPT_COMMAND_ORIGINAL"
+}
+
+prompt_command_function() {
+	$PROMPT_COMMAND_ORIGINAL
+	$PROMPT_COMMAND_CUSTOM
 }
 
 tabname() {
