a720f4ae6265e2bec4a8d4b459dba22e17a2284e
Sebastian Sangervasi <ssangervasi@squareup.com>
2019-01-10 22:03:36 -0800

    hacking tempfiles for loading lib

--- futility/package.sh
+++ futility/package.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+export FUTILITY_VERSION='0.1.0'
+
 load_futility_package() {
   FUTILITY_PACKAGE_ROOT=$(dirname $BASH_SOURCE)
 
-  source $FUTILITY_PACKAGE_ROOT/core.sh
+  FUTILITY_PACKAGE_CORE="$FUTILITY_PACKAGE_ROOT/core.sh"
+  source "$FUTILITY_PACKAGE_CORE"
 
   FUTILITY_PACKAGE_ROOT=$(current_dir)
   FUTILITY_PACKAGE_LIB="$FUTILITY_PACKAGE_ROOT/lib"
@@ -13,14 +16,43 @@ load_futility_package() {
 }
 
 load_futility_lib() {
+  # Yes this tempfile deal is hacky,
+  #   but handles files with spaces and other odd things.
+  local lib_file_list=$(mktemp)
+  ls -1 "$FUTILITY_PACKAGE_LIB" > ${lib_file_list}
+
   local lib_file
-  for lib_file in $(ls "$FUTILITY_PACKAGE_LIB"); do
+  while read lib_file; do
     source_if_exists "$FUTILITY_PACKAGE_LIB/$lib_file" --log
-  done
+  done < ${lib_file_list}
+  rm ${lib_file_list}
 }
 
 load_futility_bin() {
-  add_to_path $FUTILITY_PACKAGE_ROOT/bin
+  FUTILITY_PACKAGE_BIN="$FUTILITY_PACKAGE_ROOT/bin"
+  add_to_path "$FUTILITY_PACKAGE_BIN"
+}
+
+futility_help() {
+  format_contents() {
+    ls -1F $1 | sed 's/^/    /'
+  }
+
+  cat <<HELP_TEXT
+futility v${FUTILITY_VERSION}
+
+Paths used by this package:
+  FUTILITY_PACKAGE_ROOT : $FUTILITY_PACKAGE_ROOT/
+  FUTILITY_PACKAGE_LIB  : $FUTILITY_PACKAGE_LIB/
+  FUTILITY_PACKAGE_BIN  : $FUTILITY_PACKAGE_BIN/
+
+Lib contents:
+$(format_contents $FUTILITY_PACKAGE_LIB)
+
+Bin contents:
+$(format_contents $FUTILITY_PACKAGE_BIN)
+
+HELP_TEXT
 }
 
 load_futility_package
