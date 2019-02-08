4e8244313231f6d6f30de4c2c5e654464aa07992
Sebastian Sangervasi <ssangervasi@squareup.com>
2019-02-08 15:39:16 -0800

    old pryrc fallback

--- profiles/pryrc.rb
+++ profiles/pryrc.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 def custom_prompt(obj, nest_level, pry_inst)
-  default_proc = Pry::Prompt[:default][:value].first
+  default_proc = default_prompt.first
   default_result = default_proc.call(obj, nest_level, pry_inst)
   abbreviate(default_result)
 end
 
 def custom_wait_prompt(obj, nest_level, pry_inst)
-  default_proc = Pry::Prompt[:default][:value].second
+  default_proc = default_prompt.second
   default_result = default_proc.call(obj, nest_level, pry_inst)
   abbreviate(default_result)
 end
@@ -19,6 +19,17 @@ def abbreviate(str, max_len: 40)
   prefix = str.first(midpoint)
   suffix = str.last(midpoint)
   "#{prefix}...#{suffix}"
+end
+
+def default_prompt
+  Pry::Prompt[:default][:value]
+rescue NoMethodError
+  Pry::DEFAULT_PROMPT
+rescue
+  [
+    proc { |obj, nest_level, _pry_inst| "woop<#{obj}:#{nest_level}> " },
+    proc { |obj, nest_level, _pry_inst| "woop<#{obj}:#{nest_level}>* " }
+  ]
 end
 
 Pry.config.prompt = [
