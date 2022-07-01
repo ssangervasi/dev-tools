52650d9f94832e4491d1c6f715232a747cd6323f
Sebastian Sangervasi <villain@harmless.dev>
2022-06-30 17:01:03 -0700

    remaster -> remain

--- plugins/docker/plugin.sh
+++ plugins/docker/plugin.sh
@@ -1,3 +1,6 @@
 
 alias d="docker"
 alias dc="docker-compose"
+
+source_if_exists /Applications/Docker.app/Contents/Resources/etc/docker
+source_if_exists /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion
