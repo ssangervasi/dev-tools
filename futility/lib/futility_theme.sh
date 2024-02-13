f354c096c15eecdfee91e8a241222933e106f9e7
Sebastian Sangervasi <ssangervasi@change.org>
2024-02-13 12:57:24 -0800

    more themes

--- futility/lib/futility_theme.sh
+++ futility/lib/futility_theme.sh
@@ -2,24 +2,48 @@
 
 FUTILITY_THEME_PURPLE="#320830"
 FUTILITY_THEME_BLUE="#101035"
+FUTILITY_THEME_BLUER="#121245"
 FUTILITY_THEME_ORANGE="#492009"
 FUTILITY_THEME_GREEN="#102807"
 
-futility_theme() {
-	FUTILITY_THEME_NAME="$1"
-	if [[ "$FUTILITY_THEME_NAME" == 'purple' ]]; then
-		FUTILITY_THEME_BG="$FUTILITY_THEME_PURPLE"
-	elif [[ "$FUTILITY_THEME_NAME" == 'blue' ]]; then
-		FUTILITY_THEME_BG="$FUTILITY_THEME_BLUE"
-	elif [[ "$FUTILITY_THEME_NAME" == 'orange' ]]; then
-		FUTILITY_THEME_BG="$FUTILITY_THEME_ORANGE"
-	elif [[ "$FUTILITY_THEME_NAME" == 'green' ]]; then
-		FUTILITY_THEME_BG="$FUTILITY_THEME_GREEN"
-	fi
+FUTILITY_THEMES='
+name   bg
+purple #320830
+purpler #500845
+blue   #101035
+bluer  #121245
+orange #492009
+green  #102807
+greener  #103812
+'
 
-	# echo printf '\x1b]11;'"$FUTILITY_THEME_BG"'\x1b\\'
+
+
+futility_theme() {
+	_futility_theme_find_line "$1" || return $YA_DUN_GOOFED
+	_futility_theme_parse_line
+
 	_futility_theme_set_bg "$FUTILITY_THEME_BG"
 }
+
+FUTILITY_THEME_LINE=''
+
+_futility_theme_find_line() {
+	local name="$1"
+	local found=$(grep -i "$name" <<<"$FUTILITY_THEMES")
+	if [[ -z "$found" ]]; then
+		echo_error "No theme matches '$1'"
+		return $YA_DUN_GOOFED
+	fi
+	FUTILITY_THEME_LINE="$found"
+}
+
+_futility_theme_parse_line() {
+	FUTILITY_THEME_ARR=($FUTILITY_THEME_LINE)
+	FUTILITY_THEME_NAME="${FUTILITY_THEME_ARR[0]}"
+	FUTILITY_THEME_BG="${FUTILITY_THEME_ARR[1]}"
+}
+
 
 _futility_theme_set_bg() {
 	printf '\x1b]11;'"$1"'\x1b\\'
